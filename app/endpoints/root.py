78025a9c1abe03c42bd50c0a3412dcf4f6107d2d
mabittar <ma_bittar@yahoo.com.br>
2022-04-06 20:52:41 -0300

    change root function and add new middleware

--- app/endpoints/root.py
+++ app/endpoints/root.py
@@ -1,10 +1,10 @@
 import logging
-from functools import lru_cache
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.database.session import get_session
 from app.settings import Settings
-from yattag import Doc
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -12,33 +12,16 @@ logger = logging.getLogger(__name__)
 router = APIRouter(tags=["root"])
 
 
-@lru_cache()
 @router.get("/")
-async def root():
-    settings = Settings()
-    doc, tag, text, line = Doc(
-        defaults={
-            'title': "Spam ML in FastAPI"
-        }
-    ).ttl()
-    doc.asis('<!DOCTYPE html>')
-    msg = f"Hello visitant, {vars(settings)}!" if not settings.BIND_ENV else "Hello visitant!"
-    line("h1", "This is a Secure API you need to create a login to use the app.")
-    with tag("html"):
-        doc.input(name="title", type="text")
-        with tag("body"):
-            with tag("h1", id="header"):
-                text()
-            with tag("button", id="btn-group"):
-                with tag("a", href='/docs'):
-                    text("SwaggerUI")
-                with tag("a", href='/redoc'):
-                    text("Redoc")
-            with tag("p", id="footer"):
-                text(f"Hello visitant, {msg}!")
+async def root(session: AsyncSession = Depends(get_session)):
+    Settings()
+    try:
+        # Try to create session to check if DB is awake
+        await session.execute("SELECT 1")
+    except Exception as e:
+        logger.error(e)
+        raise e
 
-    result = doc.getvalue()
-
-    return result
+    return {"message": "Wellcome to FastAPI Machine Learning Spam Predictor"}
 
 
---
+++ app/middlewares/__init__.py
@@ -0,0 +1,5 @@
+from .req_res import RequestResponse
+
+middlewares_list = [
+    RequestResponse
+]
